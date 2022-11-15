4dd222d7998837772fba2018f857273987e9d720
LaNs992 <106981716+LaNs992@users.noreply.github.com>
2022-11-15 21:50:40 +0300

    Добавьте файлы проекта.

---
+++ ScladLIB/bisnesSclad.cs
@@ -0,0 +1,28 @@
+﻿namespace ScladLIB
+{
+    public class bisnesSclad<SCL> where SCL : class
+    {
+        private List<SCL> ScldBL = new List<SCL>();
+
+        public bisnesSclad() { }
+        public List<SCL> Get()
+        {
+            return ScldBL;
+        }
+
+        public void Add(SCL data)
+        {
+            ScldBL.Add(data);
+        }
+        public void Remove(SCL data)
+        {
+            ScldBL.Remove(data);
+        }
+
+        public void Update(SCL id, SCL data)
+        {
+            var index = ScldBL.IndexOf(id);
+            ScldBL[index] = data;
+        }
+    }
+}
